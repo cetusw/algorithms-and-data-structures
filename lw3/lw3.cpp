c980d7cb038c4d2683bb9b6d93e226162a0c3c1b
cetusw <kugelev.m@gmail.com>
2024-11-14 23:23:58 +0300

    Дописал 3 лабу с учётом правок. Добавил обработку унарных операций, исправил последовательность операций, добавил операций

--- lw3/lw3.cpp
+++ lw3/lw3.cpp
@@ -26,6 +26,64 @@ struct Tree
     Tree() : data(""), father(nullptr), left(nullptr), right(nullptr) {}
 };
 
+bool isExpressionCorrect(const std::string &expression)
+{
+    int parenthesesLevel = 0;
+    bool lastWasOperator = true;
+
+    for (int i = 0; i < expression.length(); ++i)
+    {
+        const char ch = expression[i];
+
+        if (ch == '(')
+        {
+            ++parenthesesLevel;
+            lastWasOperator = true;
+
+            if (i + 1 < expression.length() && expression[i + 1] == ')') {
+                std::cout << "Пустые скобки в выражении" << std::endl;
+                return false;
+            }
+        }
+        else if (ch == ')')
+        {
+            --parenthesesLevel;
+            if (parenthesesLevel < 0)
+            {
+                std::cout << "В выражении нет открывающей скобки для закрывающей" << std::endl;
+                return false;
+            }
+            lastWasOperator = false;
+        }
+        else if (ch == '^' || ch == '*' || ch == '/')
+        {
+            if (lastWasOperator)
+            {
+                std::cout << "Нет операнда для операции" << std::endl;
+                return false;
+            }
+            lastWasOperator = true;
+        }
+        else
+        {
+            lastWasOperator = false;
+        }
+    }
+
+    if (parenthesesLevel > 0)
+    {
+        std::cout << "В выражении нет закрывающей скобки" << std::endl;
+        return false;
+    }
+    if (lastWasOperator)
+    {
+        std::cout << "Нет операнда для операции" << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
 void printDown(const Tree *p, const int level)
 {
     if (p == nullptr)
@@ -47,19 +105,22 @@ void printDown(const Tree *p, const int level)
 int findLowestPriorityOperatorIndex(const std::string &expression)
 {
     int lowestPriorityIndex = -1;
-    int lowestPriority = 3;
+    int lowestPriority = 4;
     int parenthesesLevel = 0;
+    std::string state = "start";
 
-    for (int i = 0; i < expression.size(); ++i)
+    for (int i = static_cast<int>(expression.size()) - 1; i > 0; --i)
     {
         const char ch = expression[i];
 
         if (ch == '(')
         {
+            state = "operand";
             ++parenthesesLevel;
         }
         if (ch == ')')
         {
+            state = "operator";
             --parenthesesLevel;
         }
 
@@ -69,14 +130,33 @@ int findLowestPriorityOperatorIndex(const std::string &expression)
 
             if (ch == '+' || ch == '-')
             {
-                currentPriority = 1;
+                if (state == "operand")
+                {
+                    currentPriority = 1;
+                    state = "operator";
+                }
+                else
+                {
+                    state = "one-operator";
+
+                }
+            }
+            else if (ch == '*' || ch == '/')
+            {
+                currentPriority = 2;
+                state = "operator";
             }
             else if (ch == '^')
             {
-                currentPriority = 2;
+                currentPriority = 3;
+                state = "operator";
+            }
+            else
+            {
+                state = "operand";
             }
 
-            if (currentPriority < lowestPriority)
+            if ((currentPriority < lowestPriority || (currentPriority == lowestPriority && currentPriority == 3)) && state != "one-operator" && state != "operand")
             {
                 lowestPriority = currentPriority;
                 lowestPriorityIndex = i;
@@ -125,16 +205,17 @@ Tree* buildExpressionTree(const std::string &expression)
 
         if (hasOuterParentheses)
         {
-            return buildExpressionTree(createSubstring(expression, 1, static_cast<int>(expression.length() - 1)));
+            return buildExpressionTree(createSubstring(expression, 1, static_cast<int>(expression.length() - 2)));
         }
     }
 
     const int index = findLowestPriorityOperatorIndex(expression);
+    std::cout << expression << " " << index << std::endl;
 
     if (index == -1)
     {
         Tree* node = new Tree();
-        node->data = expression[0];
+        node->data = expression;
         return node;
     }
 
@@ -158,6 +239,7 @@ int main()
 
         std::cout << "\nВведите имя файла (exit, чтобы выйти): ";
         std::cin >> fileName;
+        fileName = "expr.txt";
 
         if (fileName == "exit")
         {
@@ -170,8 +252,11 @@ int main()
         {
             std::string expression;
             std::getline(inputFile, expression);
-            const Tree* root = buildExpressionTree(expression);
-            printDown(root, 0);
+            if (isExpressionCorrect(expression))
+            {
+                const Tree* root = buildExpressionTree(expression);
+                printDown(root, 0);
+            }
         }
         else
         {
